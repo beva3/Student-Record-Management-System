4f175e341cb56f01e743a143d87cef73c64265fe
beva3 <mizabienvenu@gmail.com>
2024-11-05 15:09:23 +0530

    add prototype

---
+++ student.h
@@ -0,0 +1,25 @@
+#ifndef __STUDENTS__
+#define __STUDENTS__
+
+typedef struct Student{
+    char name[50];
+    int rollNumber
+    int age;
+    float grade; // student's NOTES
+}Student;
+
+// function to add a new student record 
+void addStudent(Student students[], int *count);
+// function to display all students records
+void displayStudents(const Student students[], int count);
+// function to update existing students records
+void updateStudent(Student students[], int count);
+// function to delete a student record by roll number
+void deletStudent(Student students[], int *count,int rollNumber);
+// function to search for a student record by roll number
+void searchStudent(const Student students[], int count, const char *name);
+// function to save a student to a file
+void saveRecordsToFile(const Student students[], int count, const char *fileName);
+// function to load student records from a file
+void loadRecordFromFile(Student students[], int *count, const char *fileName);
+#endif // !__STUDENTS__
